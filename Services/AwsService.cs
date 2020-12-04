ea0156f8c6ec08a59c69557fff3725fc62bbd576
Ashok Chakravarthy Thota <Ashok4994@users.noreply.github.com>
2020-12-04 14:57:44 -0800

    Add files via upload

---
+++ Services/AwsService.cs
@@ -0,0 +1,112 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JwtService.Helpers;
+using JwtService.Entities;
+
+namespace JwtService.Services
+{
+
+    public interface IAwsService
+    {
+        Task<bool> CreateQueue(string queuename);
+        Task<string> SendMessageAsync(MessageRequest request);
+        Task<List<string>> RecieveMessage(string queuename, string accountId);
+    }
+    public class AwsService : IAwsService
+    {
+        private readonly string awsAccessKeyId = "AKIAYZCL4NMT3IOJ6GVS";
+        private readonly string awsSecretAccessKey = "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";
+        private readonly IAmazonSQS _sqsClient;
+
+        public AwsService()
+        {
+             _sqsClient = new AmazonSQSClient(awsAccessKeyId, awsSecretAccessKey, RegionEndpoint.USWest2);
+        }
+
+        public async Task<bool> CreateQueue(string queuename)
+        {
+                      
+            var createRequest = new CreateQueueRequest()
+            {
+                QueueName = queuename
+            };
+
+            var createResponse = await _sqsClient.CreateQueueAsync(createRequest);
+
+            if(createResponse != null)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public async Task<string> SendMessageAsync(MessageRequest sendrequest)
+        {           
+            //Get Queue url
+            var queueUrl = GetQueueUrl(sendrequest.QueueName,sendrequest.QueueOwnerAWSAccountId);
+          
+
+            //Send message
+            var sendMessageRequest = new SendMessageRequest()
+            {
+                QueueUrl = queueUrl.Result,
+                MessageBody = sendrequest.MessageBody
+            };
+
+            var sendMessageResponse = await _sqsClient.SendMessageAsync(sendMessageRequest);
+
+            if (sendMessageResponse != null)
+            {
+                return "success";
+            };
+            return "failure";
+
+        }
+
+        public async Task<List<string>> RecieveMessage(string queuename,string accountId)
+        {
+             List<string> messages = new List<string>();
+
+            var queueUrl = GetQueueUrl(queuename, accountId);
+
+            var receiveMessageRequest = new ReceiveMessageRequest()
+            {
+                QueueUrl = queueUrl.Result,             
+            };
+
+            var receiveMessageResponse = await _sqsClient.ReceiveMessageAsync(receiveMessageRequest);
+
+            foreach(var message in receiveMessageResponse.Messages)
+            {
+                if (message.Body != null)
+                    messages.Add(message.Body);
+                    
+            }
+            return messages;
+        }
+
+        private async Task<string> GetQueueUrl(string queuename, string accountId)
+        {
+            var request = new GetQueueUrlRequest
+            {
+                QueueName = queuename,
+                QueueOwnerAWSAccountId = accountId
+            };
+
+            var queueUrlresponse =  await _sqsClient.GetQueueUrlAsync(request);
+
+            if(queueUrlresponse != null)
+            {
+                return queueUrlresponse.QueueUrl;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,83 @@
+﻿using JwtService.Entities;
+using JwtService.Helpers;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtService.Services
+{
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+    }
+
+    public class UserService : IUserService
+    {
+        public AppSettings _appSettings;
+
+        public List<User> users = new List<User>
+        {
+            new User
+            {
+                id = 1,
+                FirstName = "Test",
+                LastName = "User",
+                Password = "test"
+            }
+        };
+
+        public UserService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public User Authenticate(string username, string password)
+        {
+
+            //User Authentication
+            var user = users.SingleOrDefault(x => x.FirstName == username && x.Password == password);
+
+            if(user == null)
+            {
+                return null;
+            }
+
+            //authentication successful so generate JWTToken
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.id.ToString())
+
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            //Remove password in response
+            user.Password = null;
+            return user;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return users.Select(x =>
+            {
+                x.Password = null;
+                return x;
+            });
+        }
+    }
+}
