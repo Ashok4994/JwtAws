1d620201cc236e6cb2887886139480019e12554f
Ashok Chakravarthy Thota <Ashok4994@users.noreply.github.com>
2020-12-04 14:57:09 -0800

    Add files via upload

---
+++ Helpers/AwsClient.cs
@@ -0,0 +1,32 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtService.Helpers
+{
+    public abstract class AwsClient
+    {
+
+        public IOptionsMonitor<AwsCredentials> _awsCredentials;
+
+        private static IAmazonSQS sqsclient;
+
+        static string awsAccessKeyId = "AKIAYZCL4NMT3IOJ6GVS";
+        static string awsSecretAccessKey = "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";
+        //IAmazonSQS sqsClient = new AmazonSQSClient(awsAccessKeyId, awsSecretAccessKey, RegionEndpoint.USWest2);
+
+        public AwsClient(IOptionsMonitor<AwsCredentials> awsCredentials)
+        {
+            _awsCredentials = awsCredentials;
+            sqsclient = new AmazonSQSClient(_awsCredentials.CurrentValue.AccessKeyId, _awsCredentials.CurrentValue.SecretAccessKey, RegionEndpoint.USWest2);
+        }
+
+        protected AwsClient()
+        {
+        }
+    }
+}
---
+++ Helpers/AwsCredentials.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtService.Helpers
+{
+    public class AwsCredentials
+    {
+        public string AccessKeyId { get; set; }
+
+        public string SecretAccessKey { get; set; }
+
+    }
+}
