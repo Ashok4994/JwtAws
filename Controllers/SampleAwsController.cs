4e3cbed7fb6949950e033be0c1154b59a2b6f234
Ashok Chakravarthy Thota <Ashok4994@users.noreply.github.com>
2020-12-04 14:56:10 -0800

    Add files via upload

---
+++ Controllers/SampleAwsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JwtService.Entities;
+using JwtService.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleAwsController : ControllerBase
+    {
+        public IAwsService _awsService;
+        public SampleAwsController(IAwsService awsService)
+        {
+            _awsService = awsService;
+        }
+
+        [HttpPost]
+        [Route("create-queue/{queuename}")]
+        public async Task<bool> CreateQueue([FromRoute]string queuename)
+        {
+            var response = await _awsService.CreateQueue(queuename);
+            return response;
+        }
+
+        [HttpPost]
+        [Route("send-message")]
+        public async Task<string> SendMessage([FromBody]MessageRequest request)
+        {
+            try
+            {
+             var response = await _awsService.SendMessageAsync(request);
+
+             return response;
+
+            }
+            catch
+            {
+                return "Not created";
+            }
+        }
+    
+        [HttpGet]
+        [Route("receive-message/{queuename}")]
+        public async Task<List<string>> ReceiveMessage([FromRoute]string queuename, [FromBody]string AccountId)
+        {
+            var response = await _awsService.RecieveMessage(queuename, AccountId);
+            return response;
+        }
+
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JwtService.Entities;
+using JwtService.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]User userParam)
+        {
+            var user = _userService.Authenticate(userParam.Username, userParam.Password);
+
+            if (user == null)
+            {
+                return BadRequest(new { message = "Username or Password is incorrect" });
+
+            }
+
+            return Ok(user);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+
+        }
+
+
+
+    }
+}
