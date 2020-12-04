679e4eb26477cbcb2411d5f1a349bb13b5fd3b4e
Ashok Chakravarthy Thota <Ashok4994@users.noreply.github.com>
2020-12-04 14:56:38 -0800

    Add files via upload

---
+++ Entities/MessageRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtService.Entities
+{
+    public class MessageRequest
+    {
+        public string QueueName { get; set; }
+        public string QueueOwnerAWSAccountId { get; set; }
+        public string MessageBody { get; set; }
+    }
+}
---
+++ Entities/SQSMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtService.Entities
+{
+    public class SQSMessage
+    {
+        public Dictionary<string, string> Attributes { get; set; }     
+        public string Body { get; set; }
+    }
+}
